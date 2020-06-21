327c87d91111dfd2a434c3d9f096ff4f717c0e17
vihhugo <victor-hugo-nogueira@hotmail.com>
2020-06-21 19:46:53 -0300

    Ajuste Estrutura

--- LanchoneteFAM/LanchoneteFAM.cs
+++ LanchoneteFAM/LanchoneteFAM.cs
@@ -8,7 +8,7 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using WindowsFormsApp1.Model;
+using LanchoneteFAM.Model;
 
 namespace WindowsFormsApp1
 {
--- LanchoneteFAM/Model/Produto.cs
+++ LanchoneteFAM/Model/Produto.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WindowsFormsApp1.Model
+namespace LanchoneteFAM.Model
 {
     public class Produto
     {
